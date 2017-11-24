658db18aede3c9740474ce03e490a76bd8e46c25
William Vitorino <williamvitorino3@gmail.com>
2017-11-24 10:47:53 +0000

    Últimas alterações.

--- Basico/main.py
+++ Basico/main.py
@@ -3,7 +3,7 @@ import tkinter as tk
 # Instância
 root = tk.Tk()
 # Widgets
-btn = tk.Button(root, text='Me aperte')
+btn = tk.Button(root, text='Click me')
 # Gerenciador de Layout
 btn.pack()
 # Loop principal
